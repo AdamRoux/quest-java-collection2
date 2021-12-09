7707b6d1279156a43d9ddd165c85b0858000e605
AdamRoux <roux.adam69@gmail.com>
2021-12-09 10:28:59 +0100

    Exercice done

--- Hero.java
+++ Hero.java
@@ -1,30 +1,37 @@
-public class Hero {
+public class Hero implements Comparable<Hero> {
 
-    // attributs
-    private String name;
-    private int age;
+	// attributs
+	private String name;
+	private int age;
 
-    // constructeurs
-    public Hero(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+	// constructeurs
+	public Hero(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 
-    // accesseurs (getters)
-    public String getName() {
-        return name;
-    }
+	// accesseurs (getters)
+	public String getName() {
+		return name;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public int getAge() {
+		return age;
+	}
 
-    // mutateurs (setters)
-    public void setName(String name) {
-        this.name = name;
-    }
+	// mutateurs (setters)
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int compareTo(Hero hero) {
+		// TODO Auto-generated method stub
+		return hero.getAge() - this.getAge();
+	}
+
+
 }
--- Thanos.java
+++ Thanos.java
@@ -1,9 +1,22 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
-
+	
     public static void main(String[] args) {
+   	 
+   	 Comparator<Hero> ComparatorName = new Comparator<Hero>() {
+
+			@Override
+			public int compare(Hero hero1, Hero hero2) {
+				// TODO Auto-generated method stub
+				return hero1.getName().compareTo(hero2.getName());
+			}
+   		 
+   	 };
+   	 
 
         List<Hero> heroes = new ArrayList<>();
         heroes.add(new Hero("Hulk", 49));
@@ -12,15 +25,19 @@ public class Thanos {
         heroes.add(new Hero("Thor", 1501));
 
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
+        
 
 
         System.out.println("\nOrder by name:");
+        Collections.sort(heroes, ComparatorName);
+        
         showList(heroes);
 
         // TODO 2: Add a Comparator and sort by age (descending)
 
 
         System.out.println("\nOrder by age:");
+        Collections.sort(heroes);
         showList(heroes);
     }
 
